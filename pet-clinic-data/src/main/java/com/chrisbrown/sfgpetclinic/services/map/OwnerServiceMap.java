95875074c270816abba25ca9a54b29c72014fd8a
Chris Brown <cdb186000@gmail.com>
2019-03-31 20:29:35 -0400

    add bootstrap for data. closes #14

--- pet-clinic-data/src/main/java/com/chrisbrown/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/chrisbrown/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -2,12 +2,19 @@ package com.chrisbrown.sfgpetclinic.services.map;
 
 import com.chrisbrown.sfgpetclinic.model.Owner;
 import com.chrisbrown.sfgpetclinic.services.CrudService;
+import com.chrisbrown.sfgpetclinic.services.OwnerService;
 
-public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long>{
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService{
 
 	@Override
 	public Owner save(Owner object) {
 		return this.save(object.getId(), object);
+	}
+
+	@Override
+	public Owner findByLastName(String lastName) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
--- pet-clinic-data/src/main/java/com/chrisbrown/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/chrisbrown/sfgpetclinic/services/map/VetServiceMap.java
@@ -2,10 +2,11 @@ package com.chrisbrown.sfgpetclinic.services.map;
 
 import com.chrisbrown.sfgpetclinic.model.Vet;
 import com.chrisbrown.sfgpetclinic.services.CrudService;
+import com.chrisbrown.sfgpetclinic.services.VetService;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
-	@Override
+	
 	public Vet save(Vet object) {
 		return this.save(object.getId(), object);
 	}

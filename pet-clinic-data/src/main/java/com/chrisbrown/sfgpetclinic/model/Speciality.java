a526ebc66b52d421137823ad9afccf312908f287
Chris Brown <cdb186000@gmail.com>
2019-12-01 16:24:09 -0500

    Add Speciality, and associate Vet to Specialities. Closes #27.

---
+++ pet-clinic-data/src/main/java/com/chrisbrown/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package com.chrisbrown.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity {
+
+	private String description;
+	
+	public String getDescription() {
+		return this.description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = desciption;
+	}
+}
--- pet-clinic-data/src/main/java/com/chrisbrown/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/chrisbrown/sfgpetclinic/model/Vet.java
@@ -2,4 +2,13 @@ package com.chrisbrown.sfgpetclinic.model;
 
 public class Vet extends Person {
 
+	private Set<Speciality> specialities;
+	
+	public Set<Speciality> get Specialities() {
+		return this.specialities;
+	}
+	
+	public void setSpecialities(Set<Speciality> specialities) {
+		this.specialities = specialities;
+	}
 }

f73042077abdb176a95a7c1e6c59938c935e36e6
Chris Brown <cdb186000@gmail.com>
2019-12-07 18:14:00 -0500

    Add Pets and Contact info to owners on startup. closes #33.

--- pet-clinic-data/src/main/java/com/chrisbrown/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/chrisbrown/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -1,19 +1,75 @@
 package com.chrisbrown.sfgpetclinic.services.map;
 
+import java.util.Set;
+
 import org.springframework.stereotype.Service;
 
 import com.chrisbrown.sfgpetclinic.model.Owner;
+import com.chrisbrown.sfgpetclinic.model.Pet;
 import com.chrisbrown.sfgpetclinic.services.CrudService;
 import com.chrisbrown.sfgpetclinic.services.OwnerService;
+import com.chrisbrown.sfgpetclinic.services.PetService;
+import com.chrisbrown.sfgpetclinic.services.PetTypeService;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService{
 
-	@Override
-	public Owner save(Owner object) {
-		return super.save(object);
+	private PetTypeService  petTypeService;
+	private PetService petService;
+	
+	public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+		this.petTypeService = petTypeService;
+		this.petService = petService;
 	}
 
+	@Override
+	public Set<Owner> findAll(){
+		return super.findAll();
+	}
+	
+	@Override
+	public Owner findById(Long id) {
+		return super.findById(id);
+	}
+	
+	@Override
+	public Owner save(Owner object) {
+		
+		if(object != null) {
+			if(object.getPets() != null) {
+				object.getPets().forEach(pet ->{
+					if(pet.getPetType() != null) {
+						if(pet.getPetType().getId() == null) {
+							pet.setPetType(petTypeService.save(pet.getPetType()));
+						}
+					}else {
+						throw new RuntimeException("Pet Type is required");
+					}
+					
+					if(pet.getId() == null) {
+						Pet savedPet = petService.save(pet);
+						pet.setId(savedPet.getId());
+					}
+				});
+			}
+			
+			return super.save(object);
+		}else {
+			return null;
+		}
+		
+	}
+	
+	@Override
+	public void delete(Owner object) {
+		super.delete(object);
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		super.deleteById(id);
+	}
+	
 	@Override
 	public Owner findByLastName(String lastName) {
 		// TODO Auto-generated method stub
--- pet-clinic-web/src/main/java/com/chrisbrown/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/chrisbrown/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,9 +1,12 @@
 package com.chrisbrown.sfgpetclinic.bootstrap;
 
+import java.time.LocalDate;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.chrisbrown.sfgpetclinic.model.Owner;
+import com.chrisbrown.sfgpetclinic.model.Pet;
 import com.chrisbrown.sfgpetclinic.model.PetType;
 import com.chrisbrown.sfgpetclinic.model.Vet;
 import com.chrisbrown.sfgpetclinic.services.OwnerService;
@@ -35,17 +38,38 @@ public class DataLoader  implements CommandLineRunner{
 		PetType cat = new PetType();
 		cat.setName("Cat");
 		PetType savedCatPetType = petTypeService.save(cat);
+		System.out.println("Loading PetTypes....");
 
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Michael");
 		owner1.setLastName("Weston");
+		owner1.setAddress("123 Brickerel");
+		owner1.setCity("Miami");
+		owner1.setTelephone("1234567890");
 		
+		Pet mikesPet = new Pet();
+		mikesPet.setPetType(savedDogPetType);
+		mikesPet.setOwner(owner1);
+		mikesPet.setBirthDate(LocalDate.now());
+		mikesPet.setName("Rosco");
+		
+		owner1.getPets().add(mikesPet);
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Fiona");
 		owner2.setLastName("Glenanne");
+		owner2.setAddress("123 Brickerel");
+		owner2.setCity("Miami");
+		owner2.setTelephone("1234567890");
 		
+		Pet fionasCat = new Pet();
+		fionasCat.setName("Just Cat");
+		fionasCat.setOwner(owner2);
+		fionasCat.setBirthDate(LocalDate.now());
+		fionasCat.setPetType(savedCatPetType);
+		
+		owner2.getPets().add(fionasCat);
 		ownerService.save(owner2);
 		
 		System.out.println("Loaded Owners....");

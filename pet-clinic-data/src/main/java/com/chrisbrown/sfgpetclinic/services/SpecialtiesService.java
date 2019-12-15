2d8f49511dd2af8baece5f16a217d7fa5c6cbe10
Chris Brown <cdb186000@gmail.com>
2019-12-14 19:05:53 -0500

    Add Specialties Service Closes#31

---
+++ pet-clinic-data/src/main/java/com/chrisbrown/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,7 @@
+package com.chrisbrown.sfgpetclinic.services;
+
+import com.chrisbrown.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long>{
+
+}
